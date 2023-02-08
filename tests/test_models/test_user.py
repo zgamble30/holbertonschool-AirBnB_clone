a3492b5b27dbd7feebb788672f0f97f1388efd32
zgamble30 <you@example.com>
2023-02-07 17:51:53 -0800

    added unit testing

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Unittest for class User
+"""
+import unittest
+from models.user import User
+
+
+class Test_classUser(unittest.TestCase):
+    """ Class that include all class User tests """
+
+    def test_attrs(self):
+        """ to check the attributes of an instances """
+        usertest = User()
+        self.assertEqual(usertest.email, "")
+        self.assertEqual(usertest.password, "")
+        self.assertEqual(usertest.first_name, "")
+        self.assertEqual(usertest.last_name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
